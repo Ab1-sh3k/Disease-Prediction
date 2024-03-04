132b7db2fc39309c6cc2d2ba0276214707474889
Ab1-sh3k <abishek.sapkota2020@gmail.com>
2024-03-04 13:35:58 +0545

    changed some settings

--- diseasepredction/settings.py
+++ diseasepredction/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-44_vgyqx+k(ha1n-9nt($fj%)bc=5xn^)x6k7qr^e0o3n__nxv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["sillycrow.pythonanywhere.com", "127.0.0.1"]
 
